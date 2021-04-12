cb8f8ef9e088ed8c9f1fcc4a21803c5be6e80199
maniteja <pratamaniteja66@gmail.com>
2021-04-12 13:01:44 +0530

    fixed minor bug

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+require("dotenv").config({ path: "./.env" });
 mongoose.Promise = global.Promise;
 let isConnected;
 module.exports = connectToDatabase = () => {
@@ -8,11 +9,24 @@ module.exports = connectToDatabase = () => {
   }
   console.log("=> using new database connection");
   return mongoose
-    .connect(process.env.DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((db) => {
-      isConnected = db.connections[0].readyState;
-    });
+    .connect(
+      "mongodb://" +
+        process.env.COSMOSDB_HOST +
+        ":" +
+        process.env.COSMOSDB_PORT +
+        "/" +
+        process.env.COSMOSDB_DBNAME +
+        "?ssl=true&replicaSet=globaldb",
+      {
+        auth: {
+          user: process.env.COSMOSDB_USER,
+          password: process.env.COSMOSDB_PASSWORD,
+        },
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        retryWrites: false,
+      }
+    )
+    .then((db) => (isConnected = db.connections[0].readyState))
+    .catch((err) => console.error(err));
 };
